b13877c17d0f9576a9e717ae0814727a1d07d244
alfaniferdiansah <ferdiansah1808@gmail.com>
2022-12-18 19:08:40 +0700

    first commit project in new CRA

---
+++ src/styling/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+        <div className="main-info">
+            <Typed
+                className="typed-text1"
+                strings={['hello my name is ferdi, i build things for the web. i am a software engineer specializing. i am focussed on building responsive app']}
+                typeSpeed={40}
+            />
+            <h1>list of my current skill as fullstack web development</h1>
+            <Typed 
+                className="typed-text2"
+                strings={["HTML/CSS", "JavaScript", "Bootstrap", "ReactJS", "NodeJS", "MongoDB", "MySQL"]}
+                typeSpeed={60}
+                backSpeed={60}
+                loop
+            />
+            <a href="#" className="btn-main">contact me</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
